85dd3d0b9faebf02b9dc907fe8d49a8f034035a5
Student <example@example.com>
2020-01-15 15:08:13 -0800

    add subraction, multiply, and division functions to the front-end user interface as well as input boxes on the home-page.

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,48 @@ var multiply = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+//user interface (or front-end) logic:
 
+$(document).ready(function()  {
+  $("form#add").submit(function(event)  {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#add-output").text(result);
+  });
+});
 
+$(document).ready(function(){
+  $("form#subtract").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#subtract-output").text(result);
+  });
+});
 
+$(document).ready(function(){
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#multiply-output").text(result);
+  });
+});
 
-//user logic
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter another number"));
-  
-  
-  var result = divide(number1, number2);
-  alert(result);
+$(document).ready(function(){
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#divide-output").text(result);
+  });
+});
+
+// var result = divide(number1, number2);
+//   alert(result);
 
